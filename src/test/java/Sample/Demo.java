a60a679ac72c5e0713acff8df82c56f7215c6420
ASHWINI <ASHWINI@LAPTOP-LPETIPEA>
2023-08-06 14:34:41 +0530

    Demo is committed

---
+++ src/test/java/Sample/Demo.java
@@ -0,0 +1,14 @@
+package Sample;
+
+import org.testng.annotations.Test;
+
+public class Demo
+{
+	@Test
+   public void sample()
+   {
+	   System.out.println("hi this ia my first commit ");
+	   
+	   System.out.println("hello");
+   }
+}
